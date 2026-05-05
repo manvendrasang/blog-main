72f7ae66c283a5c40d95fe16744ad20088f498d8
Manvendra Sang <email>
2026-05-05 22:15:18 +0530

    user session management done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,28 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext, useState, useEffect } from "react";
+import { lookInSession } from "./common/session";
+
+const UserContext = createContext();
 
 const App = () => {
+    const [userAuth, setUserAuth] = useState(null);
+    useEffect(() => {
+        let userInSession = sessionStorage.getItem("user");
+        userInSession ? setUserAuth(JSON.parse(userInSession)) : setUserAuth({ access_token: null });
+    }, [])
     return (
-        <Routes>
-            <Route path="/" element={<Navbar />}>
-                <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
-                <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
-            </Route>
-        </Routes>
+        <UserContext.Provider value={{ userAuth, setUserAuth }}>
+            <Routes>
+                <Route path="/" element={<Navbar />}>
+                    <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+                    <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
+                </Route>
+            </Routes>
+        </UserContext.Provider>
     )
 }
 
+export { UserContext };
 export default App;
