971b47a47dc57a04e6ef86fd65878af377ff9d32
Manvendra Sang <manvendrasang@gmail.com>
2026-05-10 20:24:23 +0530

    Editor and Form route done

---
+++ frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,7 @@
+const BlogEditor = () => {
+    return (
+        <h1>Blog editor component</h1>
+    )
+}
+
+export default BlogEditor;
--- frontend/src/pages/editor.pages.jsx
+++ frontend/src/pages/editor.pages.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
+import { UserContext } from "../App";
+import { Navigate } from "react-router-dom";
+import BlogEditor from "../components/blog-editor.component";
+import PublishForm from "../components/publish-form.component";
+
 const Editor = () => {
+    const [ editorState, setEditorState ] = useState("editor"); 
+    let { userAuth: { access_token } } = useContext(UserContext);
     return (
-        <h1>Editor page component</h1>
+        access_token === null ? <Navigate to="/signin" /> 
+        : editorState == "editor" ? <BlogEditor /> : <PublishForm />
     )
 }
 
