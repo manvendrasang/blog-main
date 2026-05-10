489743e30bfc56fc84cd30ad955b34ae7c6c2d5f
Manvendra Sang <manvendrasang@gmail.com>
2026-05-10 12:43:12 +0530

    Setup editor route done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar.component";
 import UserAuthForm from "./pages/userAuthForm.page";
 import { createContext, useState, useEffect } from "react";
 import { lookInSession } from "./common/session";
+import Editor from "./pages/editor.pages";
 
 const UserContext = createContext();
 
@@ -15,6 +16,7 @@ const App = () => {
     return (
         <UserContext.Provider value={{ userAuth, setUserAuth }}>
             <Routes>
+                <Route path="/editor" element={<Editor />} />
                 <Route path="/" element={<Navbar />}>
                     <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
                     <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
---
+++ frontend/src/pages/editor.pages.jsx
@@ -0,0 +1,7 @@
+const Editor = () => {
+    return (
+        <h1>Editor page component</h1>
+    )
+}
+
+export default Editor;
